bcce81a7d448002da2c7bb0354a288765b808bb4
luiarthur <luiarthur@gmail.com>
2016-03-24 12:29:19 -0700

    yes

--- randomForest/R/random_forest.R
+++ randomForest/R/random_forest.R
@@ -2,7 +2,7 @@ library(randomForest)
 
 p <- 3
 n <- 1000
-x <- matrix(rnorm(n*p,0,3),n,p)
+x <- matrix(rnorm(n*p,0,1),n,p)
 
 mu <- x[,1]^2 + sin(x[,2]) - 3*x[,3]
 y <- mu + rnorm(n,0,.1)
@@ -12,10 +12,11 @@ one.tree <- tree(y~x)
 plot(one.tree,col="grey"); text(one.tree)
 
 ranf <- randomForest(x,y)
-
 plot(y,ranf$pred)
-
 mean((ranf$pred - y)^2)
 
 plot(sort(mu),type="l")
 points(ranf$pred[order(mu)],col="blue",cex=.5)
+points(y[order(mu)]-ranf$pred[order(mu)],col="red",cex=.5)
+
+plot(y-ranf$pred,col="red",cex=.5)
